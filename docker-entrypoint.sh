ce4f82012f8588777e089dd23a9c43314cfc1635
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2020-07-19 06:11:44 +0900

    コンソールの出力を抑止

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ $(nodenv local) ]; then
+nodenv local > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    local_node_version=$(nodenv local)
     # Change node version
-    nodenv install -f `nodenv local`
-    nodenv local `nodenv local`
+    nodenv install -f $local_node_version > /dev/null 2>&1
+    nodenv local $local_node_version > /dev/null 2>&1
 fi
 
 # Execute command from argument
